ae109e3eb25035b2990923f68a5c68a519a94ecf
Valentina Carrington <Valentina_Carrington@comcast.com>
2020-08-16 16:15:07 -0600

    Added pagination to menu page

--- src/main/webapp/app/menu/bakery-items/bakery-items.component.ts
+++ src/main/webapp/app/menu/bakery-items/bakery-items.component.ts
@@ -13,8 +13,15 @@ import { BakeryItemService } from 'app/entities/bakery-item/bakery-item.service'
     styleUrls: ['bakery-items.component.scss']
 })
 export class BakeryItemsComponent implements OnInit {
+    allBakeryItems: IBakeryItem[];
     bakeryItemsCol1: IBakeryItem[];
     bakeryItemsCol2: IBakeryItem[];
+    totalItems: any;
+    queryCount: any;
+    itemsPerPage = 20;
+    page = 1;
+    previousPage: any;
+    category: string;
 
     constructor(
         private menuService: MenuService,
@@ -26,19 +33,30 @@ export class BakeryItemsComponent implements OnInit {
 
     ngOnInit() {
         this.activatedRoute.params.subscribe(params => {
+            this.category = params.category;
             this.loadAll(params.category);
         });
     }
 
     loadAll(category) {
-        this.menuService.getItemsByCategory(category, {}).subscribe(
+        this.menuService.getItemsByCategory(category, { page: this.page - 1, size: this.itemsPerPage }).subscribe(
             (res: HttpResponse<IBakeryItem[]>) => {
+                this.allBakeryItems = res.body;
+                this.totalItems = parseInt(res.headers.get('X-Total-Count'), 10);
+                this.queryCount = this.totalItems;
                 const half = Math.ceil(res.body.length / 2);
                 this.bakeryItemsCol1 = res.body.slice(0, half);
                 this.bakeryItemsCol2 = res.body.slice(half, res.body.length);
             },
             (res: HttpErrorResponse) => this.onError(res.message)
         );
+    }
+
+    loadPage(page: number) {
+        if (page !== this.previousPage) {
+            this.previousPage = page;
+            this.loadAll(this.category);
+        }
     }
 
     navigateToItemDetails(id) {
